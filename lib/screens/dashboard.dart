e894cc0dc69249dd38e71c1c71b62e87827c2e83
PinKevin <emeriokevin2320@gmail.com>
2023-12-21 00:42:15 +0700

    Coba buat logika pilih shift

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -9,15 +9,62 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  bool shift1Selected = false;
+  bool shift2Selected = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('WII'),
         ),
-        body: const Center(
-          child: Text('Selamat datang di WII'),
-        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              onPressed: () {
+                setState(() {
+                  shift1Selected = true;
+                  shift2Selected = false;
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: shift1Selected
+                    ? MaterialStateProperty.all(Colors.blue)
+                    : MaterialStateProperty.all(Colors.transparent),
+              ),
+              child: const Text('Shift 1'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+              onPressed: () {
+                setState(() {
+                  shift1Selected = false;
+                  shift2Selected = true;
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: shift2Selected
+                    ? MaterialStateProperty.all(Colors.blue)
+                    : MaterialStateProperty.all(Colors.transparent),
+              ),
+              child: const Text('Shift 2'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: (shift1Selected || shift2Selected)
+                    ? () {
+                        print('Halo');
+                      }
+                    : null,
+                child: const Text('Pilih Shift'))
+          ],
+        )),
         bottomNavigationBar: BottomBar.showBottomAppBar(context));
   }
 }
